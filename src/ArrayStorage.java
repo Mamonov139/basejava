da3f0a7693aeb787951f2e0caff331b9626f488d
Mamonov139 <velikiynovgorod1993@mail.ru>
2020-10-20 14:00:31 +0300

    Выполнено тестовое задание, реализованы методы класса ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,80 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
+
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        int i = 0;
+        while (storage[i] != null && i != storage.length) {
+            storage[i] = null;
+            i++;
+
+        }
+
     }
 
     void save(Resume r) {
+        int i = 0;
+        while (storage[i] != null && i != storage.length) {
+            i++;
+        }
+
+        storage[i] = r;
+
+
     }
 
     Resume get(String uuid) {
+        int i = 0;
+        while (storage[i] != null && i != storage.length) {
+            if (storage[i].toString() == uuid) {
+                return storage[i];
+            }
+            i++;
+        }
+
+
         return null;
     }
 
     void delete(String uuid) {
+
+        int i = 0;
+        while (storage[i] != null && i != storage.length) {
+            if (storage[i].toString().equals(uuid)) {
+                while (storage[i] != null && i != storage.length - 1) {
+                    storage[i] = storage[i + 1];
+                    i++;
+
+                }
+                storage[i] = null;
+                break;
+            }
+            i++;
+        }
+
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int l;
+        l = this.size();
+
+        return Arrays.copyOf(storage, l);
     }
 
     int size() {
-        return 0;
+        int i = 0;
+        while (storage[i] != null && i != storage.length) {
+            i++;
+        }
+        return i;
     }
 }
