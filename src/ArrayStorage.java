f95256a0d6e8d53f5b47def6f7ba98c45625fb6a
Mamonov139 <velikiynovgorod1993@mail.ru>
2020-10-23 13:57:13 +0300

    Исправления замечаний от 22.10.2020

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,76 +5,64 @@ import java.util.Arrays;
  */
 
 public class ArrayStorage {
+
     Resume[] storage = new Resume[10000];
+    private int size = 0;
+
+    public int getSize() {
+        return size;
+    }
+
+    private void setSize(int size) {
+        this.size = size;
+    }
 
     void clear() {
-        int i = 0;
-        while (storage[i] != null && i != storage.length) {
-            storage[i] = null;
-            i++;
-
+        while (this.getSize() != 0) {
+            storage[this.getSize() - 1] = null;
+            this.setSize(this.getSize() - 1);
         }
-
     }
 
     void save(Resume r) {
-        int i = 0;
-        while (storage[i] != null && i != storage.length) {
-            i++;
-        }
-
-        storage[i] = r;
-
-
+        storage[this.getSize()] = r;
+        this.setSize(this.getSize() + 1);
     }
 
     Resume get(String uuid) {
-        int i = 0;
-        while (storage[i] != null && i != storage.length) {
-            if (storage[i].toString() == uuid) {
+        for (int i = 0; i < this.getSize(); i++) {
+            if (storage[i].toString().equals(uuid)) {
                 return storage[i];
             }
-            i++;
         }
-
-
         return null;
     }
 
     void delete(String uuid) {
-
         int i = 0;
-        while (storage[i] != null && i != storage.length) {
+        while (i <= this.getSize()) {
             if (storage[i].toString().equals(uuid)) {
                 while (storage[i] != null && i != storage.length - 1) {
                     storage[i] = storage[i + 1];
                     i++;
-
                 }
                 storage[i] = null;
+                this.setSize(this.getSize() - 1);
                 break;
             }
             i++;
         }
-
-
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        int l;
-        l = this.size();
-
-        return Arrays.copyOf(storage, l);
+        return Arrays.copyOf(storage, this.getSize());
     }
 
     int size() {
-        int i = 0;
-        while (storage[i] != null && i != storage.length) {
-            i++;
-        }
-        return i;
+        return this.getSize();
     }
+
 }
