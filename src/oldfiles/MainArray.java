0330101a43941e4c09a70886ec174830628bc20a
Mamonov139 <velikiynovgorod1993@mail.ru>
2020-10-27 19:41:55 +0300

    HM2 без метода Update

--- src/oldfiles/MainArray.java
+++ src/oldfiles/MainArray.java
@@ -1,9 +1,14 @@
+package oldfiles;
+
+import urise.webapp.model.Resume;
+import urise.webapp.storage.ArrayStorage;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
- * Interactive test for ArrayStorage implementation
+ * Interactive test for urise.webapp.storage.ArrayStorage implementation
  * (just run, no need to understand)
  */
 public class MainArray {
@@ -32,7 +37,7 @@ public class MainArray {
                     break;
                 case "save":
                     r = new Resume();
-                    r.uuid = uuid;
+                    r.setUuid(uuid);
                     ARRAY_STORAGE.save(r);
                     printAll();
                     break;
---
+++ src/urise/webapp/storage/ArrayStorage.java
@@ -0,0 +1,75 @@
+package urise.webapp.storage;
+
+import urise.webapp.model.Resume;
+
+import java.util.Arrays;
+
+/**
+ * Array based storage for Resumes
+ */
+
+public class ArrayStorage {
+
+    private Resume[] storage = new Resume[10000];
+    private int size = 0;
+
+    public void clear() {
+        Arrays.fill(storage, null);
+        size = 0;
+    }
+
+    public void save(Resume r) {
+        if (size == storage.length) {
+            System.out.println("Error: not enough place ");
+            return;
+        }
+        if (findResume(r.toString()) != -1) {
+            System.out.println("Error: uuid already exist ");
+            return;
+        }
+        storage[size] = r;
+        size++;
+    }
+
+    public Resume get(String uuid) {
+        int i = findResume(uuid);
+        if (i == -1) {
+            System.out.println("Error: uuid not found ");
+            return null;
+        }
+        return storage[i];
+    }
+
+
+    public void delete(String uuid) {
+        int i = findResume(uuid);
+        if (i == -1) {
+            System.out.println("Error: uuid not found ");
+            return;
+        }
+        storage[i] = storage[size-1];
+        storage[size-1] = null;
+        size--;
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    public Resume[] getAll() {
+        return Arrays.copyOf(storage, size);
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private int findResume(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
