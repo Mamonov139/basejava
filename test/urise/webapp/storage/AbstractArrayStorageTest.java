7ca5447024cb861c966280d454d1215de446740d
Mark Tolubaev <kazimir2336415@yandex.ru>
2020-11-15 01:22:06 +0300

    HW4 Реализация тестов II, 15.11.2020

--- test/urise/webapp/storage/AbstractArrayStorageTest.java
+++ test/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -1,6 +1,5 @@
 package urise.webapp.storage;
 
-// import java.lang.reflect.Field;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -10,7 +9,6 @@ import urise.webapp.exception.NotExistStorageException;
 import urise.webapp.exception.StorageException;
 import urise.webapp.model.Resume;
 
-// import java.lang.reflect.Field;
 
 public abstract class AbstractArrayStorageTest {
 
@@ -40,14 +38,7 @@ public abstract class AbstractArrayStorageTest {
 
     @Test(expected = StorageException.class)
     public void storageOverflow() throws Exception {
-        /*storage.clear();
-        AbstractArrayStorage.STORAGE_LIMIT
-        Field field = storage.getClass().getSuperclass().getDeclaredField("STORAGE_LIMIT");
-        field.setAccessible(true);
-        int storageLimit = (int) field.get(storage);
-         */
-        // for (int i = 0; i < storageLimit; i++) {
-        for (int i = 0; i < AbstractArrayStorage.STORAGE_LIMIT; i++) {
+        for (int i = 3; i < AbstractArrayStorage.STORAGE_LIMIT; i++) {
             Resume r = new Resume();
             try {
                 storage.save(r);
